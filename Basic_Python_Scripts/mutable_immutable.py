4f9fee0d926199764ebfd222b2f8e7460cc2e70f
gauravarege1 <gaurav.rege@hotmail.com>
2023-02-06 22:30:00 +0530

    new_update_with_loops

--- Basic_Python_Scripts/mutable_immutable.py
+++ Basic_Python_Scripts/mutable_immutable.py
@@ -1,4 +1,16 @@
+## File and Elif 
 a=int(input("Enter your numberA :"))
 b=int(input("Enter your NumberB :"))
 c=int(input("Enter your numberC :"))
 print (a+b/c)
+
+## for Loop
+name="enter your name"
+for i in name:
+    print (i ,end=(" "))
+    
+### While loop
+i=1
+while i <10:
+    print ("hello word")
+    i+=1
