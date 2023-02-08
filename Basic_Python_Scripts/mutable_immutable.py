c4ca557e143735129f237dbb3f204335c80000e8
gauravarege1 <gaurav.rege@hotmail.com>
2023-02-08 15:33:21 +0530

    for_loop_update

--- Basic_Python_Scripts/mutable_immutable.py
+++ Basic_Python_Scripts/mutable_immutable.py
@@ -1,16 +1,21 @@
 ## File and Elif 
-a=int(input("Enter your numberA :"))
-b=int(input("Enter your NumberB :"))
-c=int(input("Enter your numberC :"))
-print (a+b/c)
+# a=int(input("Enter your numberA :"))
+# b=int(input("Enter your NumberB :"))
+# c=int(input("Enter your numberC :"))
+# print (a+b/c)
 
 ## for Loop
-name="enter your name"
-for i in name:
-    print (i ,end=(" "))
+#name="enter your name"
+# for i in "dinesh":
+#     print ('hello')
+#### for varriable in squance  ( this is defaination for for loop)
+
+for i in range (10):
+    print (i)
+
     
 ### While loop
-i=1
-while i <10:
-    print ("hello word")
-    i+=1
+# i=1
+# while i <10:
+#     print ("hello word")
+#     i+=1
