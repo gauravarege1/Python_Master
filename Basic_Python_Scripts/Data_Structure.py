1a69f08d9dbfc8098fc0c1cb831da4bbffa9a70a
gauravarege1 <gaurav.rege@hotmail.com>
2023-02-08 22:47:59 +0530

    update_data_Structure

--- Basic_Python_Scripts/Data_Structure.py
+++ Basic_Python_Scripts/Data_Structure.py
@@ -4,5 +4,8 @@ x= [45,abc,4.5]
 type(x)
 object are always saved in index ( it's always start from 0)
 ''' 
-x= [45,'abc',4.5]
-print (x[0:-2])
+x= [45,'abc',4.5,78,96]
+y=['ac',78,96,"pf"]
+z=[x+y]
+print (y[-1])
+#print (78 in z)
