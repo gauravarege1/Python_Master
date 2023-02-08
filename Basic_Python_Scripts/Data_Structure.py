c5cc743b4beae8413c4841c02d1f7760004e61dd
gauravarege1 <gaurav.rege@hotmail.com>
2023-02-08 22:19:51 +0530

    Data_Structure

---
+++ Basic_Python_Scripts/Data_Structure.py
@@ -0,0 +1,8 @@
+''' List  :-  collection of object (object means  string,int,flot,complex any  types of data are saved in list and refrence 
+by [] )
+x= [45,abc,4.5]
+type(x)
+object are always saved in index ( it's always start from 0)
+''' 
+x= [45,'abc',4.5]
+print (x[0:-2])
