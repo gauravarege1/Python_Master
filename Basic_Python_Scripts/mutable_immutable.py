4064598aed14be5f8124ade16268f3de472af7a7
gauravarege1 <gaurav.rege@hotmail.com>
2023-02-08 15:36:58 +0530

    for_loop_update

--- Basic_Python_Scripts/mutable_immutable.py
+++ Basic_Python_Scripts/mutable_immutable.py
@@ -8,7 +8,7 @@
 #name="enter your name"
 # for i in "dinesh":
 #     print ('hello')
-#### for varriable in squance  ( this is defaination for for loop)
+#### for varriable in squance  ( this is defaination for for loop in squnace :- list, dict,str,tupple,object)
 
 for i in range (10):
     print (i)
