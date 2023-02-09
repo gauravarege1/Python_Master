a4f694fae74ed11c93d5f2a5ee582e6c8ee83529
gauravarege1 <gaurav.rege@hotmail.com>
2023-02-09 17:00:19 +0530

    final_updae_Lst

--- Basic_Python_Scripts/Data_Structure.py
+++ Basic_Python_Scripts/Data_Structure.py
@@ -4,8 +4,19 @@ x= [45,abc,4.5]
 type(x)
 object are always saved in index ( it's always start from 0)
 ''' 
-x= [45,'abc',4.5,78,96]
-y=['ac',78,96,"pf"]
-z=[x+y]
-print (y[-1])
+# x= [45,'abc',4.5,78,96]
+# y=['ac',78,96,"pf"]
+# z=[x+y]
+# print (y[-1])
 #print (78 in z)
+## update and delete
+
+# list1=[45,'a',8,9]
+# list1[-1]='alok'
+# del list1[-2]
+# print(list1) 
+
+
+list1=[45,85,54,898,82,'alok',82,82]
+list1.remove(82)
+print (list1)
