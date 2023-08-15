752d8abcf4312b8a7eb94e38c1a53a64efe25ea1
gaurav rege <gaurav.rege@zebra.com>
2023-08-15 17:06:23 +0530

    backlog

--- Basic_Python_Scripts/new.py
+++ Basic_Python_Scripts/new.py
@@ -4,7 +4,7 @@ for i in "dinesh":
      print ('hello')
 # #### for varriable in squance  ( this is defaination for for loop in squnace :- list, dict,str,tupple,object)
 
- for i in range (1,10):
-     if i==8:
-         continue    
-     print ('hello allow ')
+#  for i in range (1,10):
+#      if i==8:
+#          continue    
+#      print ('hello allow ')
