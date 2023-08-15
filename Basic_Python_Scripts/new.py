96e8c82eb8d59df63d3b143416247cdebb19895c
gaurav rege <gaurav.rege@zebra.com>
2023-08-15 17:19:20 +0530

    modift_new_file

--- Basic_Python_Scripts/new.py
+++ Basic_Python_Scripts/new.py
@@ -1,10 +1,10 @@
 for Loop
 name="enter your name"
 for i in "dinesh":
-     print ('hello')
+     print ('hello team')
 # #### for varriable in squance  ( this is defaination for for loop in squnace :- list, dict,str,tupple,object)
 
 #  for i in range (1,10):
 #      if i==8:
 #          continue    
-#      print ('hello allow ')
+#      print ('hello team  ')
