258a232863ec73e91bb798f9de720332bae8a45f
gaurav rege <gaurav.rege@zebra.com>
2023-08-15 18:11:45 +0530

    n

--- Basic_Python_Scripts/new.py
+++ Basic_Python_Scripts/new.py
@@ -9,10 +9,10 @@ for i in "dinesh":
 #          continue    
 #      print ('hello team  ')
 
-''' 
-x= [45,'abc',4.5,78,96]
-y=['ac',78,96,"pf"]
-z=[x+y]
-print (z)
-print (y[-1])
-print (78 in y)
+# ''' 
+# x= [45,'abc',4.5,78,96]
+# y=['ac',78,96,"pf"]
+# z=[x+y]
+# print (z)
+# print (y[-1])
+# print (78 in y)
