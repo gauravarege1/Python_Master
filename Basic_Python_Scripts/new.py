0aebac3dc9dec0f20563fa48ccbe5287594099f1
gaurav rege <gaurav.rege@zebra.com>
2023-08-15 17:22:22 +0530

    final_update

--- Basic_Python_Scripts/new.py
+++ Basic_Python_Scripts/new.py
@@ -1,7 +1,7 @@
 for Loop
 name="enter your name"
 for i in "dinesh":
-     print ('hello team')
+     print ('how are you')
 # #### for varriable in squance  ( this is defaination for for loop in squnace :- list, dict,str,tupple,object)
 
 #  for i in range (1,10):
